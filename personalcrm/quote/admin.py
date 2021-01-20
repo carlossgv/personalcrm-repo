a2d5d91729366d7eb579d0f60ed9b668c4f5fcd5
Carlos Gonzalez <carlossgv@gmail.com>
2021-01-19 23:06:52 -0300

    part of models created

---
+++ personalcrm/quote/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Quote, Product, Company, Contact
+
+admin.site.register(Quote)
+admin.site.register(Product)
+admin.site.register(Company)
+admin.site.register(Contact)
