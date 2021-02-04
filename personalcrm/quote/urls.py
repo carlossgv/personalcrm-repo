9c629d08b711dff76d68b0ee3cf86c0b413b1b50
Carlos Gonzalez <carlossgv@gmail.com>
2021-02-04 20:06:40 -0300

    view site done and printing implemented

--- personalcrm/quote/urls.py
+++ personalcrm/quote/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.home, name="quote-home"),
     path("create-quote", views.create_quote, name="create-quote"),
     path("edit-quote/<int:quote_id>", views.edit_quote, name="edit-quote"),
+    path("view-quote/<int:quote_id>", views.view_quote, name="view-quote"),
     # JSON routes
     path(
         "request-product-options",
@@ -17,4 +18,5 @@ urlpatterns = [
         name="product-info",
     ),
     path("quote-index", views.get_quote_index, name="quote-index"),
+    path("quote-products/<int:quote_id>", views.get_quote_products, name="quote-products"),
 ]
